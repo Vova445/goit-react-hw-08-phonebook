222bcc6089768a33b937cc759dd888dc37790df1
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-01-21 23:47:40 +0200

    result

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Phonebook from './PhoneBook/Phonebook';
 import Register from './Registration/Registration';
 import Login from './Registration/Login';
@@ -9,11 +9,12 @@ export const App = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%', background: 'linear-gradient(90deg, #3498db, #85bdc1)' }}>
       <Navigation />
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '100px', }}>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '100px' }}>
         <Routes>
           <Route path="/" element={<Phonebook />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </div>
